04c9a4d303292bebc455ba46e6275611303c244e
Eugene Trofimovich <trofimovich.eugene.dev@gmail.com>
2021-02-22 14:56:38 +0100

    Create App; Add tweaks for mac os

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log("Hello world!");
+const { app, Browser, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    backgroundColor: "white",
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile("index.html");
+  win.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});

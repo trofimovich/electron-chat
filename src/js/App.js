31b6487cd02289ff919c4628755f8e3e9a64e9b7
Eugene Trofimovich <trofimovich.eugene.dev@gmail.com>
2021-02-24 01:40:59 +0100

    Add basic routing

--- src/js/App.js
+++ src/js/App.js
@@ -1,8 +1,31 @@
 import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 import HomeView from "./views/Home";
 
 const App = () => {
-  return <HomeView />;
+  return (
+    <Router>
+      <div className="content-wrapper">
+        <Switch>
+          <Route path="/settings">
+            <h1>Settings View</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login View</h1>
+          </Route>
+          <Route path="/register">
+            <h1>Register View</h1>
+          </Route>
+          <Route path="/">
+            <HomeView />
+          </Route>
+        </Switch>
+      </div>
+      <Navbar />
+    </Router>
+  );
 };
 
 export default App;
--- src/js/components/Navbar.js
+++ src/js/components/Navbar.js
@@ -1,27 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => (
   <div className="chat-navbar">
     <nav className="chat-navbar-inner">
       <div className="chat-navbar-inner-left">
-        <a href="/" className="btn btn-outline-success ml-2">
+        <Link to="/settings" className="btn btn-outline-success ml-2">
           Settings
-        </a>
+        </Link>
       </div>
       <div className="chat-navbar-inner-right">
         <span className="logged-in-user">Hi User</span>
-        <button
+        <Link
+          to="/register"
           onClick={() => {}}
           className="btn btn-sm btn-outline-danger ml-2"
         >
-          Logout
-        </button>
-        <button
+          Register
+        </Link>
+        <Link
+          to="/login"
           onClick={() => {}}
           className="btn btn-sm btn-outline-success ml-2"
         >
           Login
-        </button>
+        </Link>
       </div>
     </nav>
   </div>

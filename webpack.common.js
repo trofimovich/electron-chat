bd04e8154f524dbde9a93985ee8dbd0e9d42107f
Eugene Trofimovich <trofimovich.eugene.dev@gmail.com>
2021-03-03 14:21:22 +0100

    Connect to firestore

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   mode: "development",
@@ -33,7 +34,7 @@ module.exports = {
       },
     ],
   },
-  plugins: [],
+  plugins: [new Dotenv()],
   resolve: {
     extensions: [".js"],
   },

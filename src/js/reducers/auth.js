55e73ad5956728f86e9c203a3191340f067ed155
Eugene Trofimovich <trofimovich.eugene.dev@gmail.com>
2021-03-13 21:01:29 +0100

    Auth reducer refactoring

--- src/js/reducers/auth.js
+++ src/js/reducers/auth.js
@@ -1,27 +1,38 @@
-const DEFAULT_STATE = {
-  user: null,
-  isChecking: false,
+import { combineReducers } from "redux";
+
+const createAuthReducer = () => {
+  const user = (state = null, action) => {
+    switch (action.type) {
+      case "AUTH_ON_INIT":
+      case "AUTH_ON_ERROR":
+        return null;
+      case "AUTH_ON_SUCCESS":
+        return action.user;
+      default:
+        return state;
+    }
+  };
+
+  const isChecking = (state = false, action) => {
+    switch (action.type) {
+      case "AUTH_ON_INIT":
+      case "AUTH_LOGIN_INIT":
+      case "AUTH_REGISTER_INIT":
+      case "AUTH_LOGOUT_INIT":
+        return true;
+      case "AUTH_LOGOUT_SUCCESS":
+      case "AUTH_ON_SUCCESS":
+      case "AUTH_ON_ERROR":
+        return false;
+      default:
+        return state;
+    }
+  };
+
+  return combineReducers({
+    user,
+    isChecking,
+  });
 };
 
-const authReducer = (state = DEFAULT_STATE, action) => {
-  switch (action.type) {
-    case "AUTH_LOGIN_INIT":
-    case "AUTH_REGISTER_INIT":
-    case "AUTH_LOGOUT_INIT":
-      return { ...state, isChecking: true };
-    case "AUTH_LOGIN_SUCCESS":
-    case "AUTH_REGISTER_SUCCESS":
-    case "AUTH_LOGOUT_SUCCESS":
-      return { ...state, isChecking: false };
-    case "AUTH_ON_INIT":
-      return { user: null, isChecking: true };
-    case "AUTH_ON_SUCCESS":
-      return { user: action.user, isChecking: false };
-    case "AUTH_ON_ERROR":
-      return { user: null, isChecking: false };
-    default:
-      return state;
-  }
-};
-
-export default authReducer;
+export default createAuthReducer();
